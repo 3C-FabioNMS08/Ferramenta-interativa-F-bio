d2616d65249bd025c7d27244128307e7246bc386
3C-FabioNMS08 <00001093358580sp@al.educacao.sp.gov.br>
2024-11-08 10:10:29 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Espanha?',
+    'A capital da Espanha é Madrid'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tchau em Inglês?',
+    'tchau em ingles é bye (byai)'
+)
